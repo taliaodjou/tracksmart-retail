5e41fd59f7332817ac7687d47f714e0157e829ef
Talia Odjou <talia.odjou@gmail.com>
2026-05-20 20:37:22 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -244,7 +244,7 @@ export default function Dashboard() {
   ];
 
   return (
-    <div className="min-h-screen bg-secondary/30 pb-24 sm:pb-0 pt-14 sm:pt-16">
+    <div className="min-h-screen bg-secondary/30 pb-24 sm:pb-0 pt-16 sm:pt-20">
       {needsOnboarding && !onboardingDone && (
         <OnboardingModal user={user} onComplete={() => setOnboardingDone(true)} />
       )}
